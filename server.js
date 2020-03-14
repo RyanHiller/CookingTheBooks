060f95ecb31eb27caeaad31ebce13e87e31e7fb2
Ryan Hiller <allamericanslacker@gmail.com>
2020-03-14 14:29:45 -0600

    Redirected express app to client folder

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(express.static('./client/cooking_the_books/build/'));
 
 // Mount routes
 app.use('/api/recipes/', require('./server/routes/recipes-route'));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+  res.sendFile('index.html', { root: __dirname + '/client/cooking_the_books/build/'});
 });
 
 const { PORT } = process.env;
