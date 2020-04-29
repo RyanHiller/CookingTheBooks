632d92f238e9922fd9d362153ae716ad4daa9269
Ryan Hiller <allamericanslacker@gmail.com>
2020-04-29 12:51:21 -0600

    Made suggestive search slightly more efficient

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,45 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import styles from './SearchBar.module.css';
+import styles from "./SearchBar.module.css";
 
-const SearchBar = props => {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  const [searchResults, setSearchResults] = React.useState([]);
+const SearchBar = (props) => {
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
 
-  const handleChange = event => {
-    setSearchTerm(event.target.value);
-  };
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
 
-  React.useEffect(() => {
-    const results = getSuggestions(searchTerm);
-    setSearchResults(results);
-  }, [searchTerm]);
+    React.useEffect(() => {
+        const results = getSuggestions(searchTerm);
+        setSearchResults(results);
+    }, [searchTerm]);
 
-  const getSuggestions = input => {
-    const inputValue = input.trim().toLowerCase();
-    const inputLength = inputValue.length;
+    const getSuggestions = (input) => {
+        const inputValue = input.trim().toLowerCase();
+        const inputLength = inputValue.length;
+        let filteredArr = [];
 
-    return inputLength === 0
-      ? []
-      : props.recipes.filter(recipe => recipe.tags.includes(inputValue) || recipe.title.toLowerCase().slice(0, inputLength) === inputValue);
-  };
+        if (inputLength < 3) {
+          return [];
+        } else {
+          filteredArr = props.recipes.filter(recipe => recipe.title.toLowerCase().includes(inputValue));
+          /* TODO: Add ingredient search */
+        }
 
-  return (
-    <div className={styles.Search}>
-      <div className={styles.SearchBar}>
-        <input className={styles.SearchInput} type="text" placeholder="Search by recipe or ingredient" value={searchTerm} onChange={handleChange} />
-        <div className={styles.SearchButton}>Search</div>
-      </div>
-      <ul className={styles.SearchSuggestions}>
-        {searchResults.map(item => (
-          <li className={styles.SearchSuggestion} key={item._id}>
-            <Link to={`/recipes?id=${item._id}`}>{item.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+
+        if (filteredArr.length > 7) filteredArr = filteredArr.slice(0, 7);
+        return filteredArr;
+    };
+
+    return (
+        <div className={styles.Search}>
+            <div className={styles.SearchBar}>
+                <input
+                    className={styles.SearchInput}
+                    type='text'
+                    placeholder='Search by recipe or ingredient'
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+                <div className={styles.SearchButton}>Search</div>
+            </div>
+            <ul className={styles.SearchSuggestions}>
+                {searchResults.map((item) => (
+                    <li className={styles.SearchSuggestion} key={item._id}>
+                        <Link to={`/recipes?id=${item._id}`}>{item.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default SearchBar;
