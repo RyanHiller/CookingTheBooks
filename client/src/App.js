05a4d100525c87de34dda11e5532f155f06826f9
Ryan Hiller <allamericanslacker@gmail.com>
2020-04-30 15:40:50 -0600

    Cleanup and minor fixes

--- client/src/App.js
+++ client/src/App.js
@@ -5,31 +5,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // Pages
 import HomePage from './containers/HomePage/HomePage';
 import RecipePage from './containers/RecipePage/RecipePage';
+import SearchResultsPage from './containers/SearchResultsPage/SearchResultsPage';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recipes: [],
-    };
-  }
-
-  componentDidMount() {
-    //axios.get('/api/recipes').then(res => this.setState({ recipes: res.data })).catch(alert);
-  }
-
-  render() {
-    const { recipes } = this.state;
-
-    return (
-      <div className='App'>
-        <Router>
-          <Route path='/' exact render={() => <HomePage recipes={recipes} />} />
-          <Route path={`/recipes`} render={() => <RecipePage recipes={recipes} recipe={null} />} />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App'>
+      <Router>
+        <Route path='/' exact render={() => <HomePage />} />
+        <Route path='/recipes' render={() => <RecipePage recipe={null} />} />
+        <Route path='/search' render={() => <SearchResultsPage />} />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
